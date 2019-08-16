bfc217ed87bf6fdc5686b1499234b6d0d4f3dab2
Takhir Nafikov <takhir.learn@gmail.com>
2019-08-16 19:38:01 +0300

    created response classes simple testing

--- app/src/main/java/com/takhir/foodrecipes/RecipeListActivity.java
+++ app/src/main/java/com/takhir/foodrecipes/RecipeListActivity.java
@@ -1,12 +1,72 @@
 package com.takhir.foodrecipes;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.takhir.foodrecipes.models.Recipe;
+import com.takhir.foodrecipes.requests.RecipeApi;
+import com.takhir.foodrecipes.requests.ServiceGenerator;
+import com.takhir.foodrecipes.requests.responses.RecipeSearchResponse;
+import com.takhir.foodrecipes.util.Constants;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class RecipeListActivity extends BaseActivity {
+
+    private static final String TAG = "RecipeListActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_list);
+
+        findViewById(R.id.test).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                testRetrofit();
+            }
+        });
+    }
+
+    private void testRetrofit() {
+        RecipeApi recipeApi = ServiceGenerator.getRecipeApi();
+
+        Call<RecipeSearchResponse> responseCall = recipeApi
+                .searchResponse(
+                        Constants.API_KEY,
+                        "chicken breast",
+                        "1"
+                );
+        responseCall.enqueue(new Callback<RecipeSearchResponse>() {
+            @Override
+            public void onResponse(Call<RecipeSearchResponse> call, Response<RecipeSearchResponse> response) {
+                Log.d(TAG, "onResponse:" + response.toString());
+                if(response.code() == 200) {
+                    Log.d(TAG, "onResponse:" + response.body().toString());
+                    List<Recipe> recipes = new ArrayList<>(response.body().getRecipes());
+                    for (Recipe recipe : recipes) {
+                        Log.d(TAG, "onResponse:" + recipe.getTitle());
+                    }
+                } else {
+                    try {
+                        Log.d(TAG, "onResponse" + response.errorBody().string());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<RecipeSearchResponse> call, Throwable t) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/takhir/foodrecipes/requests/RecipeApi.java
+++ app/src/main/java/com/takhir/foodrecipes/requests/RecipeApi.java
@@ -1,4 +1,25 @@
 package com.takhir.foodrecipes.requests;
 
+import com.takhir.foodrecipes.models.Recipe;
+import com.takhir.foodrecipes.requests.responses.RecipeResponse;
+import com.takhir.foodrecipes.requests.responses.RecipeSearchResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface RecipeApi {
+
+    @GET("api/search")
+    Call<RecipeSearchResponse> searchResponse(
+        @Query("key") String key,
+        @Query("q") String query,
+        @Query("page") String page
+    );
+
+    @GET("api/get")
+    Call<RecipeResponse> getRecipe(
+        @Query("key") String key,
+        @Query("rId") String recipe_id
+    );
 }
